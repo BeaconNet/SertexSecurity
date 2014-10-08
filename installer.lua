1bc38ada4f693abd8ced4dd0ce92bc18f839ed8d
Ale2610 <ale.proto00@gmail.com>
2014-10-08 08:03:28 +0200

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,1 +1,11 @@
 os.pullEvent = os.pullEventRaw
+
+--https://raw.githubusercontent.com/Sertex-Team/SertexSecurity/master/security.lua
+
+if fs.exists("/startup") then
+  fs.rename("startup", "startup.bak")
+elseif fs.exists("/startup") and fs.exists("/startup.bak")
+  fs.delete("startup.bak")
+  fs.rename("startup", "startup.bak")
+end
+
