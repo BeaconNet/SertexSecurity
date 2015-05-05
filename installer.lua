cad0514df4c04c53c4aa0dedcebff1af7991c789
Ale2610 <Ale2610>
2015-05-05 19:51:24 +0200

    ._.

--- installer.lua
+++ installer.lua
@@ -189,33 +189,6 @@ end
 
 --END SHA256
 
---SertexAPI by Ale2610
---1.0
-
-function right(y, text )
-        w, h = term.getSize()
-        term.setCursorPos(w - #text, y)
-        write(text)
-end
-
-function left(y, text)
-	term.setCursorPos(1, y)
-	write( text )
-end
-
-function center(y, text )
-        local w, h = term.getSize()
-        term.setCursorPos((w - #text) / 2, y)
-        write(text)
-end
-
-function centerDisplay( text )
-        local x, y = term.getSize()
-        term.setCursorPos(( x - string.len(text)) / 2, y / 2)
-        write( text )
-end
-
--- end sapi
 
 if term.isColour() then
   bg = colors.white
